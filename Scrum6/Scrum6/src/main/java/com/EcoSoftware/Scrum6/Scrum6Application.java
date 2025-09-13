3fd7992ddf9d5adce46b33a51b9fdc8041a39147
Paula Sepulveda <paula06sepulveda@gmail.com>
2025-09-13 07:44:27 -0500

    Archivos actualizados

---
+++ Scrum6/Scrum6/src/main/java/com/EcoSoftware/Scrum6/Scrum6Application.java
@@ -0,0 +1,13 @@
+package com.EcoSoftware.Scrum6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scrum6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scrum6Application.class, args);
+	}
+
+}
