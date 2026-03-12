871c6047674558e22542e26d6c7c941adf69c822
Dana Castro <danacastro2014@gmail.com>
2026-03-12 14:25:51 -0500

    Correciones

--- Scrum6/Scrum6/src/main/java/com/EcoSoftware/Scrum6/Config/SecurityConfig.java
+++ Scrum6/Scrum6/src/main/java/com/EcoSoftware/Scrum6/Config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.EcoSoftware.Scrum6.Config;
 
-import com.EcoSoftware.Scrum6.Security.JwtAuthFilter;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +17,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.List;
+import com.EcoSoftware.Scrum6.Security.JwtAuthFilter;
 
 @Configuration
 @EnableWebSecurity
--- Scrum6/Scrum6/src/main/java/com/EcoSoftware/Scrum6/Controller/RouteController.java
+++ Scrum6/Scrum6/src/main/java/com/EcoSoftware/Scrum6/Controller/RouteController.java
@@ -1,7 +1,5 @@
 package com.EcoSoftware.Scrum6.Controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/api/route")
